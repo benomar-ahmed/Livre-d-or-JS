ab8afdac6192add9cb96df1f6016950fbaf15599
benomar-ahmed <ben-omar.ahmed@laplateforme.io>
2023-02-14 14:19:33 +0100

    Function register finish

--- Classes/Users.php
+++ Classes/Users.php
@@ -1,13 +1,10 @@
 <?php
 
-class users {
+class Users {
    
     private $id;
-    public $login;
-    public $password;
-    public $email;
-    public $firstname;
-    public $lastname;
+    private $login;
+    private $password;
     public $pdo;
     public $msg_error;
 
@@ -16,13 +13,10 @@ class users {
         $this->pdo = new PDO('mysql:host=localhost;dbname=livreorjs;charset=utf8','root','');
     }
 
-    public function register($login,$password,$email,$firstname,$lastname)
+    public function register($login,$password)
     {
         $this->login = $login;
         $this->password = $password;
-        $this->email =  $email;
-        $this->firstname = $firstname;
-        $this->lastname = $lastname;
 
         $requete = $this->pdo->prepare("SELECT login FROM utilisateurs WHERE login=:login");
         $requete->execute(array(':login' => $login));
@@ -35,19 +29,22 @@ class users {
 
         else 
         {
-            $requete2 = $this->pdo->prepare("INSERT INTO utilisateurs (`login`,`password`,`email`,`firstname`,`lastname`) VALUES (?,?,?,?,?)");
-            $requete2->execute(array('$login','$password','$email','$firstname','$lastname'));
-            echo "Tu es connexté !";
-
+            $requete2 = $this->pdo->prepare("INSERT INTO utilisateurs (`login`,`password`) VALUES (:login,:password)");
+            $requete2->execute(array(':login' => $login,':password' => $password));
+            echo "Tu es connecté !";
+            header('Location: connexion.php');
         }
 
     }
 
 
-    // public function connec()
-    // {
+    public function connect($login,$password)
+    {
+        $this->login = $login;
+        $this->password = $password;
 
-    // }
+        $requete = $this->pdo->prepare("SELECT login and password FROM utilisateurs ");
+    }
 }
 
 
---
+++ inscription.php
@@ -0,0 +1,60 @@
+<?php
+
+require 'Classes/Users.php';
+
+if(isset($_POST['submit'])){
+
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $verify = $_POST['verify-password'];
+
+    if($password == $verify){
+        $newuser = new Users();
+        $newuser->register($_POST['login'],$_POST['password']);
+    }
+
+    else {
+        echo "Les mots de passe sont différents !";
+    }
+
+    
+}
+
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+    <main>
+        <h1>Inscription</h1>
+
+        <form action="" method="post">
+
+            <label for="login">Votre Login :</label>
+            <input type="text" name="login" required>
+
+            <label for="password">Votre mot de passe :</label>
+            <input type="password" name="password" id="" required>
+
+            <label for="verify-password">Retapez votre mot de passe :</label>
+            <input type="password" name="verify-password" id="" required>
+
+            <input type="submit" value="Créer l'utilisateur" name="submit">
+
+        </form>
+    </main>
+</body>
+</html>
