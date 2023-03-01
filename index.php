16306a934ba3af180bf2f70a1bf896af328c2fad
benomar-ahmed <ben-omar.ahmed@laplateforme.io>
2023-03-01 15:28:52 +0100

    Try with file js

--- index.php
+++ index.php
@@ -5,11 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
+    <script src="script.js" defer></script>
 </head>
 <body>
     <header>
         <?php require 'header.php' ?>
-    </header>
-    <h1>Page d'accueil !</h1>
+    <main>
+        <h1>Page d'accueil !</h1>
+
+        <button id="connexion">
+            Connexion
+        </button>
+    </main>
+    
+
+
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,8 @@
+let BtnConnexion = document.getElementById('connexion');
+
+
+BtnConnexion.addEventListener('click', connexion);
+
+function connexion(){
+    console.log('Tu as appuyé sur connexion !');
+}
